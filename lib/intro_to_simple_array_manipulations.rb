3a9c08b5f1899d819cc2b41d796d2ba4b3f41e3e
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 06:27:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,14 +1,32 @@
-def using_push (arr, str)
+def using_push(arr, str)
   arr.push(str)
 end
 
-def using_unshift (arr,str)
+def using_unshift(arr,str)
   arr.unshift(str)
 end
 
-def using_pop (arr)
+def using_pop(arr)
   arr.pop 
 end
 
 def pop_with_args(arr)
   arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift 
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, var)
+  arr.insert(var, 4)
+end
+
